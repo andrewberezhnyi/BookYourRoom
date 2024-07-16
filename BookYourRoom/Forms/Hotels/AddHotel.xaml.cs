ea62036bc3afbc2ff4871b7e3cbaf3c3a541c3bc
andrewberezhnyi <drausidrqs@gmail.com>
2024-07-16 23:31:40 +0200

    created AddHotel form

---
+++ BookYourRoom/Forms/Hotels/AddHotel.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookYourRoom.Forms.Hotels
+{
+    /// <summary>
+    /// Interaction logic for AddHotel.xaml
+    /// </summary>
+    public partial class AddHotel : Window
+    {
+        public AddHotel()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string hotelName = HotelNameTextBox.Text;
+            string hotelAddress = HotelAddressTextBox.Text;
+
+            if (string.IsNullOrEmpty(hotelName) || string.IsNullOrEmpty(hotelAddress))
+            {
+                MessageBox.Show("Please enter both hotel name and address.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            MessageBox.Show("Hotel added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
+        }
+    }
+}
